bd85ae45784407d0ddc91e24cd6a646627de28a7
rafaelovna <aikanush777@mail.ru>
2022-12-14 00:12:00 +0400

    Готово!

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
         task1();
         task1_1();
         task1_2();
+        task2();
+        task2_2();
         task3();
         task3_1();
         task3_2();
@@ -16,34 +18,45 @@ public class Main {
         numbers[0] = 1;
         numbers[1] = 2;
         numbers[2] = 3;
-        System.out.print(numbers[0] + ", ");
-        System.out.print(numbers[1] + ", ");
-        System.out.print(numbers[2] + " ");
-        System.out.println();
     }
 
     //2-ая часть
     public static void task1_1() {
         System.out.println("Задание 1_1");
         double[] arr = {1.57, 7.654, 9.986};
-        System.out.print(arr[0] + ", ");
-        System.out.print(arr[1] + ", ");
-        System.out.print(arr[2] + " ");
-        System.out.println();
     }
 
     public static void task1_2() {
         System.out.println("Задание 1_2");
         int[] januaryFriday = {6, 13, 20, 27};
-        System.out.print(januaryFriday[0] + ", ");
-        System.out.print(januaryFriday[1] + ", ");
-        System.out.print(januaryFriday[2] + ", ");
-        System.out.print(januaryFriday[3] + " ");
+
+    }
+
+    public static void task2() {
+        System.out.println("Задание 2");
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        for (int n = 0; n < numbers.length; n++) {
+            System.out.print(numbers[n]);
+            if (n != numbers.length - 1) {
+                System.out.print(", ");
+            }
+        }
         System.out.println();
     }
 
-    // Задание 2: Распечатайте на отдельной строчке элементы каждого массива по порядку через запятую.
-// В конце строки запятую ставить не надо. Выполнила в первом задании.
+    public static void task2_2() {
+        System.out.println("Задание 2_2");
+        int[] januaryFriday = {6, 13, 20, 27};
+        for (int i = 0; i < januaryFriday.length; i++) {
+            System.out.print(januaryFriday[i]);
+            if (i != januaryFriday.length-1)
+                System.out.print(", ");
+        }
+        System.out.println();
+    }
     public static void task3() {
         System.out.println("Задание 3");
         int[] numbers = new int[3];
@@ -51,16 +64,21 @@ public class Main {
         numbers[1] = 2;
         numbers[2] = 3;
         for (int i = numbers.length-1; i >= 0; i--) {
-            System.out.print(numbers[i]+" ");
+            System.out.print(numbers[i]);
+            if (i != 0){
+                System.out.print(", ");
+            }
         }
         System.out.println();
     }
-
     public static void task3_1() {
         System.out.println("Задание 3_1");
         double[] arr = {1.57, 7.654, 9.986};
         for (int i = arr.length-1; i >= 0; i--) {
-            System.out.print(arr[i] + ", ");
+            System.out.print(arr[i]);
+            if (i != 0){
+                System.out.print(", ");
+            }
         }
         System.out.println();
     }
@@ -69,7 +87,10 @@ public class Main {
         System.out.println("Задание 3_2");
         int[] januaryFriday = {6, 13, 20, 27};
         for (int i = januaryFriday.length-1; i>=0 ; i--) {
-            System.out.print(januaryFriday[i] + ", ");
+            System.out.print(januaryFriday[i]);
+            if (i != 0){
+                System.out.print(", ");
+            }
         }
         System.out.println();
     }
@@ -81,10 +102,13 @@ public class Main {
         numbers[2] = 3;
         for (int i = 0; i <= numbers.length-1; i++) {
             if (numbers[i]%2==0){
-                System.out.print(numbers[i]+", ");
+                System.out.print(numbers[i]);
             } else {
                 numbers[i]++;
-                System.out.print(numbers[i]+", ");
+                System.out.print(numbers[i]);
+            }
+            if (i != numbers.length-1){
+                System.out.print(", ");
             }
         }
         System.out.println();
@@ -94,10 +118,13 @@ public class Main {
         int[] januaryFriday = {6, 13, 20, 27};
         for (int i = 0; i <= januaryFriday.length-1; i++) {
             if (januaryFriday[i]%2==0){
-                System.out.print(januaryFriday[i]+", ");
+                System.out.print(januaryFriday[i]);
             } else {
                 januaryFriday[i]++;
-                System.out.print(januaryFriday[i]+", ");
+                System.out.print(januaryFriday[i]);
+            }
+            if (i != januaryFriday.length-1){
+                System.out.print(", ");
             }
         }
         System.out.println();
